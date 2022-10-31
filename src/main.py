02bee3936b6ec49328ee6c6066258d0351bfe308
Abayomi Adekanmbi <adekanmbiyomi@gmail.com>
2022-10-31 17:23:35 -0600

    add ip info

--- src/main.py
+++ src/main.py
@@ -1,8 +1,10 @@
 import json
 import os
+import re
 
 from flask import Flask
 from flask import request
+from urllib.request import urlopen
 app = Flask(__name__)
 
 #RESPONSE = os.environ["RESPONSE"]
@@ -14,6 +16,21 @@ def root():
 	}
 
 	return json.dumps(ret)
+
+@app.route("/ip")
+def get_ip_info():
+    
+    url = 'http://ipinfo.io/json'
+    response = urlopen(url)
+    data = json.load(response)
+    
+    ret = {
+		'ip': data['ip'],
+		'city' : data['city'],
+		'state' : data['region']
+	}
+    
+    return json.dumps(ret)
 
 if __name__ == "__main__":
 	app.run(host='0.0.0.0')
